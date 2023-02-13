71dbbd88e0505daf4b86f20c6f596a7e5b6bac49
Lean Note Teco <LGMicchele@teco.com.ar>
2023-02-13 10:48:00 -0300

    ADD Logica de audios largos y Credenciales

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const config = {
 
 // Si el audio se encuentra en una URL, establecer la URL en la propiedad "uri" del objeto "request"
 const audio = {
-    uri: 'gs://m-infra.appspot.com/public/res/PLBWX5XYGQ2B3GP7IN8Q/users/8e28/6e13/7b54/03d7/1214/3045/4007/9cd2/8e286e137b5403d71214304540079cd2.ogg'
+    //uri: 'gs://m-infra.appspot.com/public/res/PLBWX5XYGQ2B3GP7IN8Q/users/8e28/6e13/7b54/03d7/1214/3045/4007/9cd2/8e286e137b5403d71214304540079cd2.ogg'
+    uri: 'gs://storage.botmaker.com/public/res/PLBWX5XYGQ2B3GP7IN8Q/20230213-5MyJccNmNkSoRSSJKva1tHC2q3H2-IFZFE-V2hhdHNBcHAgQXVkaW8gMjAyMy0wMi0xMyBhdCAxMC4wNC4yNw==.ogg'
 };
 
 // Crear un objeto RecognitionAudio con la URL del archivo de audio
@@ -25,7 +26,13 @@ const request = {
 
 async function quickstart() {
   // Llamar al método "recognize" del cliente de Cloud Speech-to-Text y pasarle el objeto Request 
-  const [response] = await client.recognize(request);
+  // AUDIOS MENORES DE 60 SEGUNDOS
+  //const [response] = await client.recognize(request);
+
+
+  // AUDIOS MAYORES DE 60 SEGUNDOS
+  const [operation] = await client.longRunningRecognize(request);
+  const [response] = await operation.promise();
 
   // Recorrer la lista de resultados y extraer el texto transcrito
   // El resultado de la transcripción se encuentra en el campo "alternatives"
